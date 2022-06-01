8db182168fbb43fe27b4675cec308e2aa2ca3606
lbb123 <941041520@qq.com>
2022-06-01 16:00:04 +0800

    轮播图全局组件

---
+++ src/components/Carousel/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="swiper-container" ref="mySwiper">
+    <div class="swiper-wrapper">
+      <div
+        class="swiper-slide"
+        v-for="(carouse, index) in list"
+        :key="carouse.id"
+      >
+        <img :src="carouse.imgUrl" />
+      </div>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+
+    <!-- 如果需要导航按钮 -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+export default {
+  name: "Carousel",
+  props: ["list"],
+  watch: {
+    list: {
+      immediate: true,
+      handler(newvalue, oldvalue) {
+        this.$nextTick(() => {
+          var swiper = new Swiper(this.$refs.mySwiper, {
+            loop: true,
+            spaceBetween: 30,
+            centeredSlides: true,
+            autoplay: {
+              delay: 2500,
+              disableOnInteraction: false,
+            },
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          });
+        });
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
